ae8d49113d8b01fb223ad9a333a74dae4aa40bea
rcpassos <r.passos@outlook.pt>
2025-05-12 22:44:29 +0100

    add users to db

--- app/db.py
+++ app/db.py
@@ -2,5 +2,6 @@ from pymongo import MongoClient
 from app.config import settings
 
 _client = MongoClient(settings.mongodb_uri)
-events = _client.get_database("booking").get_collection("events")
-read_models = _client.get_database("booking").get_collection("read_models")
+events = _client.booking.events
+read_models = _client.booking.read_models
+users = _client.booking.users
